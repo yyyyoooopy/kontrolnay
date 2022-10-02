a97f59106e135b8cd6f53491e971091aba0388e6
Anna <filippovaanna8888@gmail.com>
2022-10-02 23:20:30 +0300

    123

---
+++ dz/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите число :");
+int array = Convert.ToInt32(Console.ReadLine());
+for(int i=3; i< 3;i++)
+{
+    return;
+}
+Console.WriteLine (array);
+
